8911aba7fc7e506d762d74bfc11caff4e5625778
Jaedin D <jaedindhatt@gmail.com>
2019-03-30 20:51:34 -0700

    Test3

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (request, response) => {
+
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });

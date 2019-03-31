e0ad6268e0c515bf07c543266e1c551c5f7ccd27
Jaedin D <jaedindhatt@gmail.com>
2019-03-30 20:43:24 -0700

    Test2

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});

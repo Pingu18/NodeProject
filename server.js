c27a315220fb34149f9855aae8e717b0bc0a858a
Jaedin D <jaedindhatt@gmail.com>
2019-03-31 02:09:40 -0700

    Login page basic html

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const hbs = require('hbs');
 const fs = require('fs');
 const port = process.env.PORT || 8080;
@@ -8,8 +9,10 @@ var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {
-
+app.get('/', function (request, response) {
+    response.render('home.hbs', {
+        title: 'Home'
+    });
 });
 
 app.listen(port, () => {

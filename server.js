a48f501e2fd14d8df011bd53c245f8cad467439e
Jaedin D <jaedindhatt@gmail.com>
2019-04-06 16:31:37 -0700

    Registering now saves data to database + encrypts password

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const saltrounds = 10;
 const port = process.env.PORT || 8080;
 
 var app = express();
@@ -8,6 +10,10 @@ var utils = require('./utils');
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function (request, response) {
     response.render('home.hbs', {
@@ -19,6 +25,38 @@ app.get('/register', function (request, response) {
     response.render('register.hbs', {
         title: 'Register'
     });
+});
+
+app.post('/create-user', function (request, response) {
+    var db = utils.getDB();
+
+    var username = request.body.username;
+    var password = request.body.password;
+    var email = request.body.email;
+
+    password = bcrypt.hashSync(password, saltrounds);
+
+    db.collection('users').find({
+        username: username
+    } || {
+        email: email
+    }).toArray(function (err, result) {
+        if (err || result[0] == null) {
+            db.collection('users').insertOne({
+                username: username,
+                password: password,
+                email: email
+            }, (err, result) => {
+                if (err) {
+                    response.send('Unable to add user')
+                }
+                response.send(JSON.stringify(result.ops, undefined, 2));
+            });
+        } else {
+            response.send('Username not available. Try again.');
+        }
+    });
+
 });
 
 app.listen(port, () => {

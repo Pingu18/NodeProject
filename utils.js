383c5c45a5b6f475c531a95ec61a0dd0ec35a92a
Jaedin D <jaedindhatt@gmail.com>
2019-04-09 22:48:40 -0700

    test

--- utils.js
+++ utils.js
@@ -13,17 +13,3 @@ module.exports.init = function (callback) {
         console.log('Succesfully connected to the MongoDB server');
     });
 };*/
-
-let uri = 'mongodb://heroku_7zxdfd6q:rv4hlanuoktj5mr3lvqv4f31mb@ds237196.mlab.com:37196/heroku_7zxdfd6q';
-
-mongodb.MongoClient.connect(uri, function (err, client) {
-
-    if (err) throw err;
-
-    /*
-     * Get the database from the client. Nothing is required to create a
-     * new database, it is created automatically when we insert.
-     */
-
-    let db = client.db('dbname');
-});
